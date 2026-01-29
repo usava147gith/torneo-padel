12dcdaf9264907f8ca88efcf1adc1bad12a20823
usava147gith <usava147@gmail.com>
2026-01-29 13:25:27 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,14 @@
+// Installazione immediata
 self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
+// Attivazione immediata
 self.addEventListener("activate", event => {
   clients.claim();
 });
 
+// Modalità sicura: NON intercetta le richieste
 self.addEventListener("fetch", event => {
-  return; // Non intercettare nulla
+  // Non fare nulla, lascia tutto al server
 });
